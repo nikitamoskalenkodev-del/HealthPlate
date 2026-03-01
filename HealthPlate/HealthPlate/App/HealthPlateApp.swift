b6be955fb8e35fdd1eb00d0367241a8c59942fe9
Nikita Moskalenko <nmoskalenko@tizbi.com>
2026-03-01 16:25:36 +0300

    Add project arch

---
+++ HealthPlate/HealthPlate/App/HealthPlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthPlateApp.swift
+//  HealthPlate
+//
+//  Created by Nikita Moskalenko on 1.03.26.
+//
+
+import SwiftUI
+
+@main
+struct HealthPlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthPlate/HealthPlate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthPlate
+//
+//  Created by Nikita Moskalenko on 1.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
